4e9ee9067ea3fbafc0c03c9f471f5c985ae2f27e
Piyush Singh <singhpiyush998@gmail.com>
2022-12-12 01:17:17 +0530

    Add: Pythonic implementation

--- ds/python/linkedlist/singly/sll.py
+++ ds/python/linkedlist/singly/sll.py
@@ -1,12 +1,24 @@
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
-
     class __Node:
         def __init__(self, data):
             self.data = data
             self.next = None
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp.data
+            temp = temp.next
+
+    def __repr__(self):
+        return f"LinkedList({', '.join(str(node) for node in self)})"
+
+    def __len__(self):
+        return len(tuple(iter(self)))
 
     @classmethod
     def getNewNode(cls, data):
@@ -17,14 +29,14 @@ class LinkedList:
         self.head = node
 
     def deleteAtHead(self):
-        if self.head is None:
-            return False
+        if (tmp := self.head) is None:
+            raise Exception("Linked List is empty")
 
         self.head = self.head.next
         return True
 
     def insertAtTail(self, node):
-        if ( tmp := self.head ) is None:
+        if (tmp := self.head) is None:
             self.head = node
         else:
             while tmp.next is not None:
@@ -32,8 +44,8 @@ class LinkedList:
             tmp.next = node
 
     def deleteAtTail(self):
-        if ( tmp := self.head ) is None:
-            return False
+        if (tmp := self.head) is None:
+            raise Exception("Linked List is empty")
 
         if tmp.next is None:
             self.head = None
@@ -43,19 +55,11 @@ class LinkedList:
             tmp.next = None
         return True
 
-    def length(self):
-        i = 0
-        tmp = self.head
-        while tmp is not None:
-            tmp = tmp.next
-            i += 1
-        return i
-
     def insertAtIndex(self, node, index):
-        if index < 0 or index > self.length():
-            return False
+        if index < 0 or index > len(self):
+            raise IndexError("Invalid index")
 
-        # 0 <= index <= self.length()
+        # 0 <= index <= len(self)
         prev = None
         curr = self.head
         for _ in range(0, index):
@@ -73,10 +77,10 @@ class LinkedList:
         return True
 
     def deleteAtIndex(self, index):
-        if index < 0 or index >= self.length():
-            return False
+        if index < 0 or index >= len(self):
+            raise IndexError("Invalid index")
 
-        # 0 <= index < self.length()
+        # 0 <= index < len(self)
         prev = None
         curr = self.head
         for _ in range(0, index):
@@ -89,45 +93,35 @@ class LinkedList:
         else:
             prev.next = curr.next
 
-        return False
-
-    def display(self):
-        if (( tmp := self.head ) is None):
-            return False
-
-        while tmp is not None:
-            print(tmp.data, end = " ")
-            tmp = tmp.next
-        print()
         return True
 
 
 def main():
     sll = LinkedList()
-    # returns true if operation is successfull else false
+    # returns true if operation is successfull
     sll.insertAtHead(LinkedList.getNewNode(6))
     sll.insertAtHead(LinkedList.getNewNode(5))
     sll.insertAtHead(LinkedList.getNewNode(3))
-    sll.display() # 3 4 5
-    print(f"Length: {sll.length()}")
+    print(sll) # 3 4 5
+    print(f"Length: {len(sll)}")
     sll.insertAtTail(LinkedList.getNewNode(7))
-    sll.display()
-    print(f"Length: {sll.length()}")
+    print(sll)
+    print(f"Length: {len(sll)}")
     sll.insertAtIndex(LinkedList.getNewNode(2), 0)
-    sll.display()
-    print(f"Length: {sll.length()}")
+    print(sll)
+    print(f"Length: {len(sll)}")
     sll.insertAtIndex(LinkedList.getNewNode(4), 2)
-    sll.display()
-    print(f"Length: {sll.length()}")
-    sll.deleteAtIndex(sll.length() - 1)
-    sll.display()
-    print(f"Length: {sll.length()}")
+    print(sll)
+    print(f"Length: {len(sll)}")
+    sll.deleteAtIndex(len(sll) - 1)
+    print(sll)
+    print(f"Length: {len(sll)}")
     sll.deleteAtIndex(0)
-    sll.display()
-    print(f"Length: {sll.length()}")
+    print(sll)
+    print(f"Length: {len(sll)}")
     sll.deleteAtIndex(2)
-    sll.display()
-    print(f"Length: {sll.length()}")
+    print(sll)
+    print(f"Length: {len(sll)}")
 
 if __name__ == "__main__":
     main()
