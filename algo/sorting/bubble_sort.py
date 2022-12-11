5f27c6e1c3ad79f4edb332ebbe5823a7c7cbf0fc
Piyush Singh <singhpiyush998@gmail.com>
2022-12-12 00:36:29 +0530

    Remove: Multiple len() calls

--- algo/sorting/bubble_sort.py
+++ algo/sorting/bubble_sort.py
@@ -1,6 +1,7 @@
 def sort(arr):
-    for i in range(0, len(arr) - 1):
-        for j in range(0, len(arr) - i - 1):
+    n = len(arr)
+    for i in range(0, n - 1):
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
