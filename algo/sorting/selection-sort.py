c4261afb25563c6d4faf1da65b62b6ac67f491d0
Piyush Singh <singhpiyush998@gmail.com>
2023-05-01 12:34:30 +0530

    FIX: typo in if

--- algo/sorting/selection-sort.py
+++ algo/sorting/selection-sort.py
@@ -2,7 +2,7 @@ def selectionSort(arr):
     for i in range(len(arr)):
         minIndex = i
         for j in range(i + 1, len(arr)):
-            if (arr[j] < arr[minIndex]):
+            if arr[j] < arr[minIndex]:
                 minIndex = j
         arr[minIndex], arr[i] = arr[i], arr[minIndex]
 
@@ -11,6 +11,7 @@ def main():
     arr = [6, 8, 3, 5, 9, 10, 7, 2, 4, 1]
     selectionSort(arr)
     print(*arr)
+
 
 if __name__ == "__main__":
     main()
