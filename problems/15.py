6aa53f7a660b28b6bb4d244f5e7facbcc9df4769
antidoid <singhpiyush998@gmail.com>
2023-09-26 21:52:49 +0530

    Add: ThreeSum

--- problems/15.py
+++ problems/15.py
@@ -6,17 +6,28 @@ Notice that the solution set must not contain duplicate triplets.
 
 def threeSum(nums: list[int]) -> list[list[int]]:
     res = []
+    nums.sort()
+    
     for i, x in enumerate(nums):
-        visited = set()
-        for y in nums[i + 1:]:
-            # two sum: if target - value not in visted then add value to visited
-            # x + y + z = 0
-            # x = -y - z => y + z = -x
-            # therefore target is -x and value is y
-            if (z := (-x - y)) in visited:
-                res.append([x, y, z])
+        if i > 0 and x == nums[i - 1]:
+            continue
+        
+        # Two sum 2: Use a left and right pointer
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            y, z = nums[l], nums[r]
+            threeSum = x + y + z
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
             else:
-                visited.add(y)
+                res.append([x, y, z])
+                r -= 1
+                while nums[r] == nums[r + 1] and l < r:
+                    r -= 1
     return res
 
+print(threeSum([-3, 3, 4, -3, 1, 2]))
 print(threeSum([-1,0,1,2,-1,-4]))
+print(threeSum([0, 0, 0, 0]))
