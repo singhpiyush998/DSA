d61532318934d95e5d651b68cb19902260f6e671
antidoid <singhpiyush998@gmail.com>
2024-07-31 16:44:31 +0530

    update: top k frequent elements

--- problems/347.py
+++ problems/347.py
@@ -1,15 +1,24 @@
-from collections import defaultdict
+"""
+Top K Frequent Elements
 
+Given an integer array nums and an integer k,
+return the k most frequent elements.
+You may return the answer in any order.
+"""
+
+# Time: O(n) Space: O(n)
 def topKFrequent(nums, k):
-    count = defaultdict(int)
+    count = {}
     for num in nums:
-        count[num] += 1
-    freq = [[] for _ in range(len(nums) + 1)]
+        count[num] = 1 + count.get(num, 0)
+
+    freq = [[] for _ in range(len(nums) + 1)] # index-count and value-items which are "count" many times
     for n, c in count.items():
         freq[c].append(n)
+
     res = []
     for i in range(len(freq) - 1, 0, -1):
         for n in freq[i]:
             res.append(n)
-        if len(res) == k:
-            return res
+            if len(res) == k:
+                return res
